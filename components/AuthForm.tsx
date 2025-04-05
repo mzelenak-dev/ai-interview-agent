6de2b382d60f85ba194a0a4ad1c5b7b7ca03d666
zedaenious <matt@zelenak.me>
2025-04-05 16:01:22 -0400

    sonner toaster; authForm schema & design; signIn <-> signUp nav

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,62 +1,87 @@
 "use client"
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
 import { z } from "zod"
+import Link from "next/link"
+import Image from "next/image"
+import { useForm } from "react-hook-form"
+import { Form } from "@/components/ui/form"
 import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { toast } from "sonner"
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
+const authFormSchema = (type: FormType) => {
+  return z.object({
+    name: type === 'sign-up' ? z.string().min(3) : z.string().optional(),
+    email: z.string().email(),
+    password: z.string().min(3),
+  });
+}
 
-const AuthForm = () => {
+const AuthForm = ({ type }: { type: FormType }) => {
+  const isSignIn = type === 'sign-in';
+  const formSchema = authFormSchema(type);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      name: '',
+      email: '',
+      password: '',
     },
   })
  
   function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values)
+    try {
+      if( type === 'sign-up') {
+        console.log('SIGN-UP', values);
+      } else if (type === 'sign-in') {
+        console.log('SIGN-IN', values);
+      } else {
+        console.log('no type provided');
+      }
+    } catch(error) {
+      console.error(error);
+      toast.error(`Error: ${error}`);
+    }
   }
 
   return (
-    <div>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
+    <section className="card-border lg:min-w-[566px]">
+      <div className="flex flex-col gap-6 card py-14 px-10">
+        <div className="flex flex-row gap-2 justify-center">
+          <Image
+            src="/logo.svg"
+            alt="logo"
+            height={32}
+            width={38}
           />
-          <Button type="submit">Submit</Button>
-        </form>
-      </Form>
-    </div>
+          <h2 className="text-primary-100">PrepSmart</h2>
+        </div>
+        <h3 className="text-center">Practice job interview with AI</h3>
+        
+        <Form { ...form }>
+          <form
+            className="w-full space-y-6 mt-4 form"
+            onSubmit={ form.handleSubmit(onSubmit) }
+          >
+            { !isSignIn && <p>Name</p> }
+            <p>Email</p>
+            <p>Password</p>
+            <Button className="btn" type="submit">
+              {isSignIn ? 'Sign In' : 'Create an Account'}
+            </Button>
+          </form>
+        </Form>
+        <p className="text-center">
+          {isSignIn ? 'No Account Yet? ' : 'Have an Account already? '}
+          <Link
+            className="font-bold text-user-primary ml-1"
+            href={ !isSignIn ? '/sign-in' : 'sign-up' }
+          >
+            {isSignIn ? 'Sign Up': 'Sign In'}
+          </Link>
+        </p>
+      </div>
+    </section>
   )
 }
 
